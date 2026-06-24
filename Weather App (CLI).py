1a6ff286571e615e1b7d3c428616f9eb8da706c2
rishi123200 <rishirishidharan116@gmail.com>
2026-06-24 18:16:16 +0530

    Create Weather App (CLI).py

---
+++ Weather App (CLI).py
@@ -0,0 +1,20 @@
+import requests
+
+API_KEY = "YOUR_API_KEY"
+
+city = input("Enter city name: ")
+
+url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
+
+response = requests.get(url)
+data = response.json()
+
+if data["cod"] == 200:
+    print("\n----- Weather Report -----")
+    print("City:", data["name"])
+    print("Temperature:", data["main"]["temp"], "°C")
+    print("Weather:", data["weather"][0]["description"])
+    print("Humidity:", data["main"]["humidity"], "%")
+    print("Wind Speed:", data["wind"]["speed"], "m/s")
+else:
+    print("City not found!")
